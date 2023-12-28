f427f92959ee00b42bbe34074cd7a30bd78c37d6
arperevalov <lovetraindriver@gmail.com>
2023-12-28 23:02:24 +0700

    feat: main handlers, connection, app_data etc.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,91 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder, web};
 use dotenv::dotenv;
-use sqlx::postgres::PgPoolOptions;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres, types::chrono};
+use serde::{Deserialize, Serialize};
+
+struct AppState {
+    db: Pool<Postgres>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct List {
+    id: i32,
+    title: String,
+    user_id: Option<i32>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Card {
+    id: i32,
+    title: String,
+    exp_date: chrono::NaiveDateTime,
+    left_count: i32,
+    units: String,
+    list_id: i32,
+    user_id: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+    id: i32,
+    username: String,
+    email: String,
+    password_hash: i32,
+    created_at: chrono::NaiveDateTime,
+}
 
 #[get("/lists")]
-async fn lists_get() -> impl Responder {
-    HttpResponse::Ok().body("Hello lists!")
+async fn lists_get(app_state: web::Data<AppState>) -> impl Responder {
+    let result = sqlx::query_as!(List, "SELECT * from lists")
+    .fetch_all(&app_state.db)
+    .await.unwrap();
+
+    HttpResponse::Ok().json(serde_json::json!(result))
 }
 
 #[get("/cards")]
-async fn cards_get() -> impl Responder {
-    HttpResponse::Ok().body("Hello cards!")
+async fn cards_get(app_state: web::Data<AppState>) -> impl Responder {
+    let result = sqlx::query_as!(Card, "SELECT * from cards")
+    .fetch_all(&app_state.db)
+    .await.unwrap();
+
+    HttpResponse::Ok().json(serde_json::json!(result))
+}
+
+#[post("/cards")]
+async fn cards_post(body: web::Json<Card>, app_state: web::Data<AppState>) -> impl Responder {
+    let result = sqlx::query_as!(Card, "SELECT * from cards")
+    .fetch_all(&app_state.db)
+    .await.unwrap();
+
+    HttpResponse::Ok().json(serde_json::json!(result))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     dotenv().ok();
     env_logger::init();
 
-    let db_url = std::env::var("DB_URL").expect("DB_URL must be set");
-    let db_username = std::env::var("DB_USERNAME").expect("DB_USERNAME must be set");
-    let db_password = std::env::var("DB_PASSWORD").expect("DB_PASSWORD must be set");
-    let db_name = std::env::var("DB_NAME").expect("DB_NAME must be set");
-    let db_connect_url = vec!["postgresql://", &db_username, ":", &db_password, "@", &db_url, "/", &db_name].join("");
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-    println!("{:?}",db_connect_url);
-    let pool = match PgPoolOptions::new().max_connections(10).connect(&db_connect_url).await {
+    let pool = match PgPoolOptions::new().max_connections(10).connect(&database_url).await {
         Ok(pool) => {
-            println!("{:?}", pool);
+            println!("CONNECTED TO DATABASE");
+            pool
         }
         Err(error) => {
-            println!("{error}")
+            println!("{error}");
+            std::process::exit(1)
         }
     };
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
+        
         App::new()
+            .app_data(actix_web::web::Data::new(AppState{db: pool.clone()}))
             .service(lists_get)
             .service(cards_get)
+            .service(cards_post)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
